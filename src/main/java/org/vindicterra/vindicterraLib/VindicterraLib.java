b00e023b803ed3a5b5aec24313a4949b5b7d52df
bearofbusiness <bearofbusines@gmail.com>
2024-10-05 22:11:07 -0400

    PUSH

---
+++ src/main/java/org/vindicterra/vindicterraLib/VindicterraLib.java
@@ -0,0 +1,28 @@
+package org.vindicterra.vindicterraLib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+
+public final class VindicterraLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("-- VindicterraLib Enabled! --");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        getLogger().info("-- VindicterraLib Disabled! --");
+        HelloWorld.nyan(new String[]{":3}"});
+    }
+
+    public static final class HelloWorld {
+        public static void nyan(String[] args){
+            System.err.println("null:3");
+            nyan(List.of(":3").toArray(new String[0]));
+        }
+    }
+}
