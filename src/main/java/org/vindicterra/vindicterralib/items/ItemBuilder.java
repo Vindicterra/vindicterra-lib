2e2d34ed0d4aba1f71e122014928966853fbb6fd
bearofbusiness <bearofbusines@gmail.com>
2024-10-09 15:55:21 -0400

    added more java docs

--- src/main/java/org/vindicterra/vindicterralib/items/ItemBuilder.java
+++ src/main/java/org/vindicterra/vindicterralib/items/ItemBuilder.java
@@ -8,33 +8,47 @@ import org.vindicterra.vindicterralib.StringUtil;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * an item builder class allowing for easy item creation
+ */
 @SuppressWarnings("ALL")
 public class ItemBuilder {
     private final ItemStack itemStack;
     private final ItemMeta meta;
-
+    /**
+     * @param material the material of the item
+     */
     public ItemBuilder(Material material) {
         this.itemStack = new ItemStack(material, 1);
         this.meta = itemStack.getItemMeta();
     }
-
+    /**
+     * param name the name of the item
+     */
     public ItemBuilder withName(String name) {
         this.meta.setDisplayName(StringUtil.formatHex(name));
         return this;
     }
-
+    /**
+     * @param amount the amount of the item
+     */
     public ItemBuilder withAmount(int amount) {
         this.itemStack.setAmount(amount);
         return this;
     }
-
+    /**
+     * @param loreLine the lore line of the item (adds to the lore)
+     */
     public ItemBuilder withLoreLine(String loreLine) {
         List<String> lore = this.meta.hasLore() ? this.meta.getLore() : new ArrayList<>();
         lore.add(StringUtil.formatHex(loreLine));
         this.meta.setLore(lore);
         return this;
     }
-
+    /**
+     * uses an elipsis to allow for multiple lore lines
+     * @param loreLine the lore line of the item (adds to the lore)
+     */
     public ItemBuilder withLore(String... loreLine) {
         List<String> lore = this.meta.hasLore() ? this.meta.getLore() : new ArrayList<>();
         for(String line : loreLine) {
@@ -44,7 +58,9 @@ public class ItemBuilder {
         this.meta.setLore(lore);
         return this;
     }
-
+    /**
+     * builds the item and returns it
+     */
     public ItemStack build() {
         this.itemStack.setItemMeta(this.meta);
         return this.itemStack;
