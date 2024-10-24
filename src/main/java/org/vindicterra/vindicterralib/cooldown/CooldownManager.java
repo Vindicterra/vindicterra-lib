9008a1c4935d2ef85e0bf9b3484149800776bb97
Shradinx <luphcore@gmail.com>
2024-10-24 19:32:29 -0400

    add getCooldown methods to cooldown manager

--- src/main/java/org/vindicterra/vindicterralib/cooldown/CooldownManager.java
+++ src/main/java/org/vindicterra/vindicterralib/cooldown/CooldownManager.java
@@ -27,19 +27,18 @@ public class CooldownManager {
             return CooldownStatus.OFF_COOLDOWN;
         }
         
-        for (Cooldown cd : cooldowns) {
-            if (cd.getPlayer().getUniqueId() != uuid) continue;
-            if (!cd.getReason().equals(reason)) continue;
-            long timeLeft = cd.getTimeLeft();
+        Cooldown pCooldown = getCooldown(player, reason);
+        if (pCooldown != null) {
+            long timeLeft = pCooldown.getTimeLeft();
             if (timeLeft > 0) {
-                cd.sendOnCooldownMessage();
+                pCooldown.sendOnCooldownMessage();
                 if (event instanceof Cancellable) {
                     ((Cancellable) event).setCancelled(true);
                 }
                 return CooldownStatus.ON_COOLDOWN;
-            } else if (timeLeft == 0) {
-                cd.remove();
-                cd.sendOffCooldownMessage();
+            } else {
+                pCooldown.remove();
+                pCooldown.sendOffCooldownMessage();
                 return CooldownStatus.OFF_COOLDOWN;
             }
         }
@@ -47,7 +46,32 @@ public class CooldownManager {
         Cooldown cd = new Cooldown(player, System.currentTimeMillis(), seconds, reason, showMessage);
         cd.runTaskTimer(VindicterraLib.getInstance(), 0, 20);
         cooldowns.add(cd);
-        
         return CooldownStatus.NEW_COOLDOWN;
+    }
+    
+    /**
+     * @param player Player to get all cooldowns for
+     * @return Possibly-empty list of active cooldowns associated with the player
+     */
+    public static List<Cooldown> getCooldowns(Player player) {
+        List<Cooldown> playerCooldowns = new ArrayList<>();
+        for (Cooldown cd : cooldowns) {
+            if (cd.getPlayer().getUniqueId() != player.getUniqueId()) continue;
+            playerCooldowns.add(cd);
+        }
+        return playerCooldowns;
+    }
+    
+    /**
+     * @param player Player to get a cooldown for
+     * @param reason Reason for cooldown
+     * @return Possibly-null active cooldown associated with player
+     */
+    public static @Nullable Cooldown getCooldown(Player player, String reason) {
+        for (Cooldown cd : getCooldowns(player)) {
+            if (!cd.getReason().equals(reason)) continue;
+            return cd;
+        }
+        return null;
     }
 }
