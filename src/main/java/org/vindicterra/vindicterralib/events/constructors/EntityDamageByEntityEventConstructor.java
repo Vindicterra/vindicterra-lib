6d4998e3041a398d60d1966a4aba81de02eedee8
Shradinx <luphcore@gmail.com>
2026-03-08 16:54:33 -0400

    update EntityDamageByEntityEventConstructor to latest version

--- src/main/java/org/vindicterra/vindicterralib/events/constructors/EntityDamageByEntityEventConstructor.java
+++ src/main/java/org/vindicterra/vindicterralib/events/constructors/EntityDamageByEntityEventConstructor.java
@@ -2,25 +2,26 @@ package org.vindicterra.vindicterralib.events.constructors;
 
 import com.google.common.base.Functions;
 import com.google.common.collect.ImmutableMap;
+import org.bukkit.damage.DamageSource;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.EnumMap;
-
 public class EntityDamageByEntityEventConstructor {
+    @SuppressWarnings({"deprecation", "UnstableApiUsage"})
     public static EntityDamageByEntityEvent entityDamageByEntityEvent(
-            @NotNull final Entity damager,
-            @NotNull final Entity damagee,
-            final @NotNull EntityDamageEvent.DamageCause damageCause,
-            final double damage,
-            final boolean isCritical
+        @NotNull final Entity damager,
+        @NotNull final Entity damagee,
+        final @NotNull EntityDamageEvent.DamageCause damageCause,
+        final @NotNull DamageSource damageSource,
+        final double damage,
+        final boolean isCritical
     ) {
         return new EntityDamageByEntityEvent(
-                damager, damagee, damageCause,
-                new EnumMap<>(ImmutableMap.of(EntityDamageEvent.DamageModifier.BASE, damage)),
-                new EnumMap<>(ImmutableMap.of(EntityDamageEvent.DamageModifier.BASE, Functions.constant(-0.0))),
+                damager, damagee, damageCause, damageSource,
+                ImmutableMap.of(EntityDamageEvent.DamageModifier.BASE, damage),
+                ImmutableMap.of(EntityDamageEvent.DamageModifier.BASE, Functions.constant(-0.0)),
                 isCritical
         );
     }
